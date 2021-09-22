45abddc448e71c5e5aa7cdc689327c9062379907
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-09-22 12:10:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,20 +61,16 @@ app.get("/", (req, res) => {
 });
 
 // Get all Movies
-app.get(
-    "/movies",
-    passport.authenticate("jwt", { session: false }),
-    (req, res) => {
-        Movies.find()
-            .then((movies) => {
-                res.status(201).json(movies);
-            })
-            .catch((err) => {
-                console.error(err);
-                res.status(500).send("Error: " + err);
-            });
-    }
-);
+app.get("/movies", function(req, res) {
+    Movies.find()
+        .then(function(movies) {
+            res.status(201).json(movies);
+        })
+        .catch(function(error) {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+        });
+});
 
 // Get movie by a title
 
