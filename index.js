c25b130e789b01e628d2d33347695c7eac3ead92
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-09-28 19:13:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -43,28 +43,7 @@ app.use(express.static("public"));
 
 //cors
 const cors = require("cors");
-let allowedOrigins = [
-    "http://localhost:8080",
-    "http://testsite.com",
-    "http://localhost:1234",
-];
-
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                // If a specific origin isn’t found on the list of allowed origins
-                let message =
-                    "The CORS policy for this application doesn’t allow access from origin " +
-                    origin;
-                return callback(new Error(message), false);
-            }
-            return callback(null, true);
-        },
-    })
-);
-
+app.use(cors());
 const { check, validationResult } = require("express-validator");
 let auth = require("./auth")(app);
 const passport = require("passport");
